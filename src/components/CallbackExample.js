810f485173fdbf43e1dd4b626205fb7f058c4e5a
Alexander Melo <alexandermelox@gmail.com>
2020-06-08 23:51:45 -0500

    Added the boilerplate code

--- src/components/CallbackExample.js
+++ src/components/CallbackExample.js
@@ -1,9 +1,14 @@
 import React, { useRef } from 'react';
 
-const CallbackExample = () => {
+const CallbackExample = ({ someValue }) => {
+  const renders = useRef(0);
+
+  // Increment on renders
+  renders.current++;
+
   return (
     <div>
-      <p>this is how many times I've rendered: {}</p>
+      <p>this is how many times I've rendered: {renders.current}</p>
     </div>
   );
 };
